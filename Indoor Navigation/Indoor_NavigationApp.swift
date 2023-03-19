0e34ecd95c2e1cb30ea41b84e48c7a2d18493ad7
Shikhar Kumar <shikharkumar009@gmail.com>
2023-03-20 04:17:24 +0530

    Initial Commit

---
+++ Indoor Navigation/Indoor_NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Indoor_NavigationApp.swift
+//  Indoor Navigation
+//
+//  Created by Shikhar Kumar on 20/03/23.
+//
+
+import SwiftUI
+
+@main
+struct Indoor_NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
